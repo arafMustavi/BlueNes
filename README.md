ccc8a9146d8adce0fcb5dab638f04b5825f55253
arafMustavi <arafmustavi@gmail.com>
2020-10-12 18:25:46 +0600

    :+1: python package xlrd working

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Nesternship-HR-Project-Medical-Data
-Medical Data extraction as a part of the ongoing nesternship program
+
+
+Medical Data extraction as a part of the ongoing NesternShip program
+
+Task 01:
+Read Data from the excel
+
+Reports on Task 1
+12/10/2020 5:55 PM : Started working with **xlrd** package of python to read the data. Following the geeksforgeeks in the following link: https://www.geeksforgeeks.org/reading-excel-file-using-python/
+
+12/10/2020 6:16 PM : The excel file can be read easily using **xlrd** packatge. Need to have a peep into **openpyxl** package
---
+++ xlrdDemo.py
@@ -0,0 +1,36 @@
+# Reading an excel file using Python
+import xlrd
+
+# Give the location of the file
+loc = ("dummydata.xlsx")
+
+# To open Workbook
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+# For row 0 and column 0
+print(sheet.cell_value(0, 0))
+
+# Print the number of column and row
+print(sheet.ncols)
+print(sheet.nrows)
+
+# Iterate through the rows and Columns
+columns = sheet.ncols
+rows = sheet.nrows
+
+dataset = []
+for i in range(rows):
+    row_data = []
+    for j in range(columns):
+        row_data.append(sheet.cell_value(i, j))
+        # print(sheet.cell(i, j))  # Prints the cells in the format 'type' : 'value'
+        # print(sheet.cell_value(i, j))  # Prints the value in the particular cells
+    dataset.append(row_data)
+
+print(dataset)
+
+# Prints the data row by row | The sheet.row_value(x) returns the sheet as a list
+for i in range(sheet.nrows):
+    print(sheet.row_values(i))
+    # print(type(sheet.row_values(i)))
